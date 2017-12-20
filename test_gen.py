15da610140d3e9a127516cc2439ea9749293c111
iida@lab <iida@nak.ics.keio.ac.jp>
2017-12-20 15:24:01 +0900

    dimentional reduction

--- test_gen.py
+++ test_gen.py
@@ -5,6 +5,30 @@ import lws
 from os.path import join
 import numpy as np
 
+def print_each_var(array, sort=False):
+    length = len(array)
+    varList = np.zeros([length, 2]) # [var, index]
+    for i in range(length):
+        varList[i,0] = np.var(array[i])
+        varList[i,1] = i
+    return varList
+
+def reduce_dim_by_var(spec):
+    # spec: ... ,513
+    # specT: 513,...
+    specT = spec.T
+    specTVar = np.zeros(len(specT)) # 513
+    for i in range(len(specT)):
+        specTVar[i] = np.var(specT[i])
+
+    # sortedSpecTVar = np.sort(specTVar)
+
+    _specT = specT
+    index = np.argsort(specTVar)
+    for i in range(len(spec)):
+       _specT[index < 100, i] = 0
+    return _specT.T
+
 def min_max(x, axis=None):
     min = x.min(axis=axis, keepdims=True)
     max = x.max(axis=axis, keepdims=True)
@@ -29,19 +53,23 @@ def save_waveform_from_spec(spectrogram, filename):
     waveform = audio.inv_spectrogram(spectrogram)
     audio.save_wav(waveform, filename)
 
-# Generate waveform from kmeans class center vector
-def main():
-    # Target data
+def load_test_data():
     mel_npy_filename = 'jsut-mel-00356.npy'
     spec_npy_filename = 'jsut-spec-00356.npy'
     label_npy_filename = 'jsut-spec-label-00356.npy'
-    filename = "120_kmeans_obj.pkl"
 
     spec = np.load(join('res/jsut', spec_npy_filename))
     label = np.load(join('res/label_120', label_npy_filename))
+
+    return spec, label
+
+# Generate waveform from kmeans class center vector
+def main():
+    # Target data
+    filename = "120_kmeans_obj.pkl"
+
     kmeans = k.load_pkl(filename)
-
-
+    spec, label = load_test_data()
 
     print("spec", spec.shape)
     print("label", label.shape)
